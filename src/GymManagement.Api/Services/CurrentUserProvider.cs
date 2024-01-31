3c107e87ded12384a3565ef07bdaed8e6f2793fd
André Lara Amorim <andre.amorim.si@gmail.com>
2024-01-31 12:24:43 -0300

    feat: consusion project

---
+++ src/GymManagement.Api/Services/CurrentUserProvider.cs
@@ -0,0 +1,33 @@
+﻿using GymManagement.Application.Common.Interfaces;
+using GymManagement.Application.Common.Models;
+using System.Security.Claims;
+using Throw;
+
+namespace GymManagement.Api.Services;
+
+public class CurrentUserProvider(IHttpContextAccessor _httpContextAccessor) : ICurrentUserProvider
+{
+    public CurrentUser GetCurrentUser()
+    {
+        _httpContextAccessor.HttpContext.ThrowIfNull();
+
+        Guid idClaim = GetClaimValues("id")
+            .Select(Guid.Parse)
+            .First();
+
+        var permissions = GetClaimValues("permissions");
+
+        var roles = GetClaimValues(ClaimTypes.Role);
+
+        return new CurrentUser(
+            Id: idClaim, 
+            Permissions: permissions,
+            Roles: roles);
+    }
+
+    private IReadOnlyList<string> GetClaimValues(string claimType) => 
+        _httpContextAccessor.HttpContext!.User.Claims
+         .Where(claim => claim.Type == claimType)
+         .Select(claim => claim.Value)
+         .ToList();
+}
---
+++ src/GymManagement.Application/Profiles/Commands/CreateAdminProfile/CreateAdminProfileCommandHandler.cs
@@ -0,0 +1,40 @@
+using ErrorOr;
+
+using MediatR;
+
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Admins;
+
+namespace GymManagement.Application.Profiles.Commands.CreateAdminProfile;
+
+public class CreateAdminProfileCommandHandler(
+    IUsersRepository _usersRepository,
+    IAdminsRepository _adminsRepository,
+    IUnitOfWork _unitOfWork,
+    ICurrentUserProvider _currentUserProvider) : IRequestHandler<CreateAdminProfileCommand, ErrorOr<Guid>>
+{
+    public async Task<ErrorOr<Guid>> Handle(CreateAdminProfileCommand command, CancellationToken cancellationToken)
+    {
+
+        var currrentUser = _currentUserProvider.GetCurrentUser();
+        if (currrentUser.Id != command.UserId)
+        {
+            return Error.Unauthorized(description: "User is forbidden from taking this action.");
+        }
+
+        var user = await _usersRepository.GetByIdAsync(command.UserId);
+        if (user is null)
+        {
+            return Error.NotFound(description: "User not foud.");
+        }
+
+        var createAdminProfileResult = user.CreateAdminProfile();
+        var admin = new Admin(userId: user.Id, id: createAdminProfileResult.Value);
+
+        await _usersRepository.UpdateAsync(user);
+        await _adminsRepository.AddAdminAsync(admin);
+        await _unitOfWork.CommitChangesAsync();
+
+        return createAdminProfileResult;
+    }
+}
